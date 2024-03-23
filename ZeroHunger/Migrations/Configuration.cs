a394371e57b2e83e57250ef4cc7e1185090dcb4f
Fahim Wayez <fahimwayez1@gmail.com>
2024-03-24 00:34:49 +0600

    Mark 02-View for Restaurant added

--- ZeroHunger/Migrations/Configuration.cs
+++ ZeroHunger/Migrations/Configuration.cs
@@ -1,18 +1,15 @@
 ﻿namespace ZeroHunger.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Zero_Hunger.Models.EF.ZeroHungerDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ZeroHunger.Models.EF.ZeroHungerDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(Zero_Hunger.Models.EF.ZeroHungerDbContext context)
+        protected override void Seed(ZeroHunger.Models.EF.ZeroHungerDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
