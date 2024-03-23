6d1a61e2c8ec6c736929da08ee6fe1042b568837
Fahim Wayez <fahimwayez1@gmail.com>
2024-03-24 02:15:56 +0600

    mark03

---
+++ ZeroHunger/Migrations/202403231932142_final.cs
@@ -0,0 +1,15 @@
+﻿namespace ZeroHunger.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class final : DbMigration
+    {
+        public override void Up()
+        {
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
